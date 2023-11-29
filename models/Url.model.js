564db0747f8dba9fcccaf56778b75b2754719e1d
illocarlos <carlosalbendiz93@gmail.com>
2023-11-29 13:14:41 +0100

    crud url

--- models/Url.model.js
+++ models/Url.model.js
@@ -1,9 +1,23 @@
 const { Schema, model } = require("mongoose");
 
-const urlSchema = new Schema(
-    {
+const urlSchema = new Schema({
+    longLink: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    nanoLink: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    id: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     }
-);
+});
 
 const Url = model("Url", urlSchema);
 
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.use("/auth", authRoutes);
 
 const userRoutes = require('./user.routes')
 router.use("/user", userRoutes);
+
+const urlRoutes = require('./url.routes')
+router.use("/url", urlRoutes);
+
 module.exports = router
